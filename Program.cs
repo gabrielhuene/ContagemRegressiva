baf0fcbbc04bf6071bcf6b1435fca4072ba993bc
gabriel huene <gabriellaurence05@gmail.com>
2022-08-26 22:30:25 -0300

    resolucão do exercício completo

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("PREPARAR...");
+Console.ReadKey();
+Console.WriteLine("APONTAR...");
+Console.ReadKey();
+Console.WriteLine("FOGO!!");
